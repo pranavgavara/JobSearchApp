a6d92d5b2444631fddb2846707453daa8ed6e2af
Pranav Gavara <Pranav Gavara>
2016-07-28 14:08:34 -0400

    Initial Project Setup

---
+++ JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/JobSearchActivity.java
@@ -0,0 +1,13 @@
+package com.jobsearchrt.jobsearchapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_search);
+    }
+}
