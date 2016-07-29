9f16a1ea83cd59580f1bb43e16a3fd05507dca8e
itengsuman <iteng.suman@gmail.com>
2016-07-28 23:23:48 -0400

    Menu added

--- JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/JobSearchActivity.java
+++ JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/JobSearchActivity.java
@@ -2,6 +2,7 @@ package com.jobsearchrt.jobsearchapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class JobSearchActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class JobSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
