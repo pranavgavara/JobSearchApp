ed0e04edf216a0ce9003903f629077b0b119a708
Pranav Gavara <Pranav Gavara>
2016-07-31 03:30:48 -0400

    full contact activity added

--- JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/FullContactActivity.java
+++ JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/FullContactActivity.java
@@ -1,13 +1,143 @@
 package com.jobsearchrt.jobsearchapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fullcontact.api.libs.fullcontact4j.FullContact;
+import com.fullcontact.api.libs.fullcontact4j.FullContactException;
+import com.fullcontact.api.libs.fullcontact4j.http.FCCallback;
+import com.fullcontact.api.libs.fullcontact4j.http.FCResponse;
+import com.fullcontact.api.libs.fullcontact4j.http.person.PersonRequest;
+import com.fullcontact.api.libs.fullcontact4j.http.person.PersonResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FullContactActivity extends AppCompatActivity {
-
+    String url = "https://api.fullcontact.com/v2/company/lookup.json?domain=";
+    String api_key = "&apiKey=ba5d7e2d4f109058";
+    TextView companyName,companyAddress,contactInfo,companyInfo;
+    ImageView companyLogo;
+    CompanyInfoDownloader companyInfoDownloader;
+    EditText domainName;
+    public class CompanyInfoObject {
+        String Name, Address, contact, company;
+        Bitmap Logo;
+    }
+    CompanyInfoObject companyInfoObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_contact);
+        companyName = (TextView) findViewById(R.id.companyName);
+        companyAddress = (TextView)findViewById(R.id.companyPlace);
+        contactInfo = (TextView) findViewById(R.id.contactInfo);
+        companyInfo = (TextView) findViewById(R.id.companyInfo);
+        companyLogo = (ImageView)findViewById(R.id.companylogo);
+        domainName= (EditText) findViewById(R.id.domainName);
+    }
+    public void submitDomain(View view) {
+        companyInfoObject=new CompanyInfoObject();
+        companyInfoDownloader=new CompanyInfoDownloader();
+        companyInfoDownloader.execute(domainName.getText().toString());
+    }
+    public class CompanyInfoDownloader extends AsyncTask<String, Void, CompanyInfoObject> {
+
+
+        protected CompanyInfoObject doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            String finalURL = url + urldisplay + api_key;
+            System.out.println(finalURL);
+
+            try {
+                URL myURL = new URL(finalURL);
+                BufferedReader br=new BufferedReader(new InputStreamReader(myURL.openConnection().getInputStream(),"UTF-8"));
+                StringBuilder stringBuilder=new StringBuilder();
+                String companyline;
+                while ((companyline=br.readLine())!=null){
+                    stringBuilder.append(companyline);
+                }
+                JSONObject queryObject=new JSONObject(stringBuilder.toString());
+                if(queryObject.getString("logo")!=null) {
+                    String imageURL = queryObject.getString("logo");
+                    InputStream in = new URL(imageURL).openStream();
+                    companyInfoObject.Logo = BitmapFactory.decodeStream(in);
+                }
+                JSONObject organizationObject=queryObject.getJSONObject("organization");
+                if(organizationObject.getString("name")!=null) {
+                    companyInfoObject.Name = organizationObject.getString("name");
+                }
+                JSONObject contactInfo=organizationObject.getJSONObject("contactInfo");
+                JSONArray address=contactInfo.getJSONArray("addresses");
+                String contacts="";
+                for(int i=0;i<address.length();i++){
+                    JSONObject contactObject=address.getJSONObject(i);
+                    if(contactObject.getString("locality")==null){
+                        continue;
+                    }else{
+                        contacts+=contactObject.getString("locality");
+                        JSONObject regionObject=contactObject.getJSONObject("region");
+                        contacts+="\n"+regionObject.getString("name");
+                        JSONObject countryObject=contactObject.getJSONObject("country");
+                        contacts+="\n"+countryObject.getString("name");
+                        i=address.length();
+                    }
+
+                }
+                if(contacts!=""){
+                    companyInfoObject.Address=contacts;
+                }
+                if(contactInfo.getJSONArray("phoneNumbers")!=null) {
+                    JSONArray phoneNumbers = contactInfo.getJSONArray("phoneNumbers");
+                    companyInfoObject.contact=phoneNumbers.getJSONObject(0).getString("number");
+                }
+                JSONArray socialArray=queryObject.getJSONArray("socialProfiles");
+                for(int j=0;j<socialArray.length();j++){
+                    JSONObject socialProfile=socialArray.getJSONObject(j);
+                    if((socialProfile.getString("typeName").equals("LinkedIn"))){ //|| (socialArray.getJSONObject(j).getString("typeName").equals("CrunchBase"))){
+                        System.out.println("h4");
+                        System.out.println(socialProfile.getString("bio"));
+                        companyInfoObject.company=socialProfile.getString("bio");
+                        break;
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return companyInfoObject;
+        }
+
+        protected void onPostExecute(CompanyInfoObject object) {
+            companyLogo.setImageBitmap(object.Logo);
+            companyName.setText(object.Name);
+            companyAddress.setText(object.Address);
+            contactInfo.setText(object.contact);
+            companyInfo.setText(object.company);
+        }
     }
 }
+
+
+
--- JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/JobSearchActivity.java
+++ JobSearchApp/app/src/main/java/com/jobsearchrt/jobsearchapp/JobSearchActivity.java
@@ -1,8 +1,10 @@
 package com.jobsearchrt.jobsearchapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class JobSearchActivity extends AppCompatActivity {
 
@@ -16,5 +18,26 @@ public class JobSearchActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch(id){
+            case R.id.FullContact:
+                Intent ContactIntent=new Intent(this,FullContactActivity.class);
+                startActivity(ContactIntent);
+                break;
+            case R.id.savedJobs:
+                Intent SavedJobsIntent=new Intent(this,SavedJobsActivity.class);
+                startActivity(SavedJobsIntent);
+                break;
+            case R.id.youtubeTips:
+                Intent YoutubeIntent=new Intent(this,InterviewTipsActivity.class);
+                startActivity(YoutubeIntent);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
